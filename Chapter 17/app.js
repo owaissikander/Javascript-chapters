3c88554397fc6929e4ac535d790fdedf14cd04f4
owaissikander <154621974+owaissikander@users.noreply.github.com>
2024-03-20 06:15:29 +0500

    chapter 17 added

---
+++ Chapter 17/app.js
@@ -0,0 +1,34 @@
+// var userInput = prompt("enter a number to show its multiplication table")
+// for (var i = 1; i <= 10; i++) {
+//     console.log(`${userInput} x ${i} =${userInput*i} `)
+// }
+
+
+
+
+
+// for(var i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+
+
+
+// fruites = ["apple", "banana", "orange", "mango", "strawberry"]
+// for (var i = 0; i < fruites.length; i++) {
+//     console.log(fruites[i]);
+
+// }
+
+
+// for(var counting = 1; counting<=15; counting++){
+//     document.write(counting,  )
+// }
+
+// for(var reverse =10; reverse>=1; reverse--){
+//     document.write(reverse)
+// }
+
+for(var even=0; even<=20; even+1){
+    document.write(even)
+}
